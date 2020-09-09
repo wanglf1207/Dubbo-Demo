ae4c6e713396b6967a7ce1e1c56210e68614ee72
104091-王利峰 <wanglf@belink.com>
2020-09-09 15:59:04 +0800

    some thing

--- dubbo-provider/src/main/java/com/test/DemoServiceImpl.java
+++ dubbo-provider/src/main/java/com/test/DemoServiceImpl.java
@@ -1,13 +1,12 @@
 package com.test;
 
 import org.springframework.stereotype.Service;
-import com.test.DemoService;
 
 @Service("demoService")
 public class DemoServiceImpl implements DemoService{
 
     @Override
     public String sayHello(String name) {
-        return "hello" + name;
-    }  
+        return "hello  " + name;
+    }
 }
